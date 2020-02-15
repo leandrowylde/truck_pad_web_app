8daa7fd2a9da44672dc7bdb758135ba5212f9eb2
leandrowylde <leandro.job@live.com>
2020-02-15 13:44:09 -0300

    Routes Update

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,32 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import NewDriver from "../views/NewDriver.vue";
+import Drivers from "../views/Drivers.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/drivers/new",
+    name: "Cadastrar Novo Motorista",
+    component: NewDriver
+  },
+  {
+    path: "/drivers",
+    name: "Motoristas cadastrados",
+    component: Drivers
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
