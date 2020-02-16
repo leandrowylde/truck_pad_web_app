411d5df6b7df92ecd20a03eedbfa4e88abe16ff1
leandrowylde <leandro.job@live.com>
2020-02-16 14:36:54 -0300

    New Driver View

--- src/views/NewDriver.vue
+++ src/views/NewDriver.vue
@@ -1,88 +1,15 @@
 <template>
-  <div>
-    <v-container fluid>
-      <v-row justify="center">
-        <v-card class="mx-auto" width="800">
-          <v-card-title>Novo motorista</v-card-title>
-          <v-card-text>
-            <v-form>
-              <v-row>
-                <v-col cols="12" md="12">
-                  <v-text-field
-                    label="Nome"
-                    color="amber"
-                    outlined
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-
-              <v-row>
-                <v-col cols="12" md="6">
-                  <v-text-field
-                    label="Telefone"
-                    color="amber"
-                    outlined
-                    v-mask="['(##) ####-####', '(##) #####-####']"
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12" md="6">
-                  <v-text-field
-                    label="Data de nascimento"
-                    color="amber"
-                    outlined
-                    v-mask="'##/##/####'"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-
-              <v-row>
-                <v-col cols="12" md="4">
-                  <v-text-field
-                    label="Registro CNH"
-                    color="amber"
-                    outlined
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12" md="4">
-                  <v-select
-                    :items="cnh_types"
-                    label="Tipo de CNH"
-                    color="amber"
-                    outlined
-                  ></v-select>
-                </v-col>
-                <v-col cols="12" md="4">
-                  <v-text-field
-                    label="CPF"
-                    color="amber"
-                    outlined
-                    v-mask="'###.###.###-##'"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-
-              <v-row>
-                <v-col cols="12" class="d-flex flex-row-reverse">
-                  <v-btn color="amber">Salvar</v-btn>
-                </v-col>
-              </v-row>
-            </v-form>
-          </v-card-text>
-        </v-card>
-      </v-row>
-    </v-container>
-  </div>
+  <driver-form></driver-form>
 </template>
 <script>
-import { mask } from "vue-the-mask";
+import DriverForm from "../components/DriverForm.vue";
+
 export default {
-  directives: {
-    mask
+  components: {
+    DriverForm
   },
   data() {
-    return {
-      cnh_types: ["A", "B", "C", "D", "E", "AB", "AC", "AD", "AE"]
-    };
+    return {};
   }
 };
 </script>
