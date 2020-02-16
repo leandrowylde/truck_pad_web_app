0274e6b1950336333d6db0ad1dd028c904ee1dcd
leandrowylde <leandro.job@live.com>
2020-02-16 14:36:47 -0300

    Edit Driver View

---
+++ src/views/EditDriver.vue
@@ -0,0 +1,15 @@
+<template>
+  <driver-form></driver-form>
+</template>
+<script>
+import DriverForm from "../components/DriverForm.vue";
+
+export default {
+  components: {
+    DriverForm
+  },
+  data() {
+    return {};
+  }
+};
+</script>
