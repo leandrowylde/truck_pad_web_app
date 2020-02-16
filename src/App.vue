4402e07c5b282a44f86f7477396090dc3a005bdc
leandrowylde <leandro.job@live.com>
2020-02-16 14:34:31 -0300

    Notification snackbar with vuex update

--- src/App.vue
+++ src/App.vue
@@ -21,19 +21,58 @@
 
     <v-content>
       <router-view></router-view>
+      <v-snackbar
+        v-model="successSnackbar"
+        color="success"
+        :timeout="3000"
+        bottom
+        right
+        >{{ successSnackbarMessage }}
+        <v-btn @click="successSnackbar = false" text dark>Fechar</v-btn>
+      </v-snackbar>
+      <v-snackbar
+        v-model="errorSnackbar"
+        color="danger"
+        :timeout="3000"
+        bottom
+        right
+        >{{ errorSnackMessage }}
+        <v-btn @click.prevent="errorSnackbar = false" text dark>Fechar</v-btn>
+      </v-snackbar>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
   name: "App",
 
   data: () => ({
-    //
+    successSnackbar: false,
+    errorSnackbar: false,
+    successSnackbarMessage: null,
+    errorSnackMessage: null
   }),
   created() {
     this.$store.dispatch("seedDrivers");
+  },
+  computed: {
+    ...mapGetters(["successMessage", "errorMessage"])
+  },
+  watch: {
+    successMessage(newValue, oldValue) {
+      if (newValue !== null) {
+        this.successSnackbar = true;
+        this.successSnackbarMessage = newValue;
+      }
+    },
+    errorMessage(newValue, oldValue) {
+      if (newValue !== null) {
+        this.errorSnackbar = true;
+        this.errorSnackMessage = newValue;
+      }
+    }
   }
 };
 </script>
